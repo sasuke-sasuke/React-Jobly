a7e7cb72ba3ae3c1dae6adedce79be23f7955d86
Jeof Petty <jeofpetty@icloud.com>
2023-04-09 18:37:08 -0400

    added curr user conditionals

--- jobly-front/src/components/NavBar.js
+++ jobly-front/src/components/NavBar.js
@@ -1,16 +1,47 @@
-import {NavLink} from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import {NavLink, Navigate} from "react-router-dom";
+
 
 export default function NavBar() {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    
+    const handleLogout = () => {
+        setCurrentUser(null);
+    }
+
     return (
         <nav>
             <ul>
-                <NavLink to='/' >Jobly</NavLink>
-                <NavLink to='/companies'>Companies</NavLink>
-                <NavLink to='/jobs'>Jobs</NavLink>
-                <NavLink to='/profile'>Profile</NavLink>
-                <NavLink to='/logout'>Logout</NavLink>
-                <NavLink to='/signup'>Signup</NavLink>
-                <NavLink to='/login'>Login</NavLink>
+                <li>
+                    <NavLink to='/' >Jobly</NavLink>
+                </li>
+                {currentUser ? 
+                <>
+                    <li>
+                        <NavLink to='/companies'>Companies</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/jobs'>Jobs</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/profile'>Profile</NavLink>
+                    </li>
+                    <li>
+                        <NavLink onClick={handleLogout} to='/'>Logout {currentUser}</NavLink>
+                    </li>
+                </>
+                
+                 :
+                 <>
+                    <li><NavLink to='/login'>Login</NavLink></li>
+                    <li><NavLink to='/signup'>Signup</NavLink></li>
+                 </>
+                 }
+                
+                
+                
+                
             </ul>
         </nav>
     )
--- jobly-front/src/pages/Homepage.js
+++ jobly-front/src/pages/Homepage.js
@@ -1,11 +1,19 @@
+import { useContext } from "react"
+import { UserContext } from "../context/UserContext"
+import Button from "../components/Button"
 
 export default function Homepage() {
+    const { currentUser } = useContext(UserContext)
     return (
         <>
             <h1>Jobly</h1>
             <p>All the jobs in one convient place</p>
-            <button>Signup</button>
-            <button>Login</button>
+            {currentUser ? <h2>Welcome Back, {currentUser}</h2> 
+            : 
+            <>
+                <Button path='/signup' text='Sign Up'/>
+                <Button path='/login' text='Log In'/>
+            </> }
         </>
     )
 }
