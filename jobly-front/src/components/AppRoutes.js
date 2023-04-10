00bedf1060aba4f24f1b7ee452826ccbd1d7d375
Jeof Petty <jeofpetty@icloud.com>
2023-04-10 01:28:31 -0400

    fixed sentax error with /companies/:handle route

--- jobly-front/src/components/AppRoutes.js
+++ jobly-front/src/components/AppRoutes.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import Companies from '../pages/Companies';
+import CompanyDetails from '../pages/CompanyDetails';
 import Jobs from '../pages/Jobs';
 import Profile from '../pages/Profile';
 import Signup from '../pages/Signup';
@@ -16,7 +17,7 @@ export default function AppRoutes() {
                 <Route path='/login' element={<Login/>} />
                 <Route element={<PrivateRoute/>} >
                     <Route path='/companies' element={<Companies/>} />
-                    <Route path='companies:handle' element={<Companies/>} />
+                    <Route path='/companies/:handle' element={<CompanyDetails/>} />
                     <Route path='/jobs' element={<Jobs/>} />
                     <Route path='/profile' element={<Profile/>} />
                 </Route>
