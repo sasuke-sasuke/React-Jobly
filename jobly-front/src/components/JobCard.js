76c5709f2e9ffac46e352bf1297e4bb228f1995f
Jeof Petty <jeofpetty@icloud.com>
2023-04-10 03:04:37 -0400

    added isTitleHidden prop

--- jobly-front/src/components/JobCard.js
+++ jobly-front/src/components/JobCard.js
@@ -1,12 +1,12 @@
 
-export default function JobCard({jobs}) {
+export default function JobCard({jobs, isTitleHidden=false}) {
 
     return (
         <>
             {jobs.map(j => (
                 <div key={j.id}>
                     <h2>{j.title}</h2>
-                    <p>{j.companyName}</p>
+                    {isTitleHidden && <p>{j.companyName}</p>}
                     <p>Salary: {j.salary}</p>
                     <p>Equity: {j.equity}</p>
                     <button>Apply</button>
