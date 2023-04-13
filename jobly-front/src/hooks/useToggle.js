3402ab8bce76f0bfc2122b4455ffd9abd517800d
Jeof Petty <jeofpetty@icloud.com>
2023-04-13 10:52:55 -0400

    .

--- jobly-front/src/hooks/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useReducer } from "react";
-
-function toggler(currentValue, newValue) {
-    return typeof newValue === "boolean"? newValue :!currentValue;
-}
-
-export default function useToggle(initialState=false) {
-    return useReducer(toggler, initialState);
-}
